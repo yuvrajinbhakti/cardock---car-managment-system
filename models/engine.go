48d334e75674b257a86def5d610859851141de96
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-05-25 12:47:23 +0530

    Adding validations for engine request

--- models/engine.go
+++ models/engine.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 )
 type Engine struct {
@@ -9,3 +11,45 @@ type Engine struct {
 	NoOfCylinders int64 `json:"no_of_cylinders"`
 	CarRange int64 `json:"car_range"`
 }
+
+type EngineRequest struct {
+	Displacement int64 `json:"displacement"`
+	NoOfCylinders int64 `json:"no_of_cylinders"`
+	CarRange int64 `json:"car_range"`
+}
+
+func ValidateEngineRequest(engineRequest EngineRequest) error {
+	if err := validateDisplacement(engineRequest.Displacement); err != nil{
+		return err
+	}
+	if err := validateNoOfCylinders(engineRequest.NoOfCylinders); err != nil{
+		return err
+	}
+	if err := validateCarRange(engineRequest.CarRange); err != nil{
+		return err
+	}
+	return nil
+}
+
+func validateDisplacement(displacement int64) error {
+	if displacement <= 0{
+		return errors.New("Displacement must be greater than 0")
+	}
+	return nil
+}
+func validateNoOfCylinders(noOfCylinders int64) error {
+	if noOfCylinders <= 0{
+		return errors.New("Number of cylinders must be greater than 0")
+	}
+	return nil
+}
+
+func validateCarRange(carRange int64) error {
+	if carRange <= 0{
+		return errors.New("Car range must be greater than 0")
+	}
+	return nil
+}
+
+
+
