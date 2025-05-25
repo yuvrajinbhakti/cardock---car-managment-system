41ba86212520a3119368f0170649e244b3ebc665
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-05-25 12:03:21 +0530

    create engine struct

--- models/engine.go
+++ models/engine.go
@@ -1,1 +1,11 @@
 package models
+
+import (
+	"github.com/google/uuid"
+)
+type Engine struct {
+	EngID uuid.UUID `json:"id"`
+	Displacement int64 `json:"displacement"`
+	NoOfCylinders int64 `json:"no_of_cylinders"`
+	CarRange int64 `json:"car_range"`
+}
