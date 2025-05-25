64ef23a9f6bc998fe7390f8ed80b643b1668417a
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-05-25 13:16:55 +0530

    creating function definitions for engine and car

--- store/car/car.go
+++ store/car/car.go
@@ -1,1 +1,46 @@
 package car
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/yuvrajinbhakti/cardock---car-managment-system/models"
+)
+
+type Store struct {	//create a struct for the store
+	db *sql.DB
+}
+
+//creating store instance with db connection
+func new(db *sql.DB) Store{ //create a new store
+	return Store{db: db}
+}
+
+// functions with db
+
+func (s Store) GetCarById(ctx context.Context, id string) (models.Car, error) {
+	// TODO: Implement car retrieval logic
+	return models.Car{}, nil
+}
+
+
+func(s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool) (models.Car, error){
+	// TODO: Implement car retrieval logic
+	return models.Car{}, nil
+}
+
+func(s Store) CreateCar(ctx context.Context, carReq *models.CarRequest)(models.Car, error){
+	// TODO: Implement car creation logic
+	return models.Car{}, nil
+}
+
+func(s Store) UpdateCar(ctx context.Context, id string, carReq *models.CarRequest)(models.Car, error){
+	// TODO: Implement car update logic
+	return models.Car{}, nil
+}
+
+func(s Store) DeleteCar(ctx context.Context, id string)(error){
+	// TODO: Implement car deletion logic
+	return nil
+}
+
--- store/engine/engine.go
+++ store/engine/engine.go
@@ -1,1 +1,42 @@
 package engine
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/yuvrajinbhakti/cardock---car-managment-system/models"
+)
+type EngineStore struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *EngineStore {
+	return &EngineStore{db: db}
+}
+
+func (e *EngineStore) GetEngineById(ctx context.Context, id string) (models.Engine, error) {
+	// TODO: Implement engine retrieval logic
+	return models.Engine{}, nil
+}
+
+func (e *EngineStore) GetEngineByBrand(ctx context.Context, brand string, isEngine bool) (models.Engine, error) {
+	// TODO: Implement engine retrieval logic
+	return models.Engine{}, nil
+}
+
+
+func (e *EngineStore) CreateEngine(ctx context.Context, engineReq *models.EngineRequest) (models.Engine, error) {
+	// TODO: Implement engine creation logic
+	return models.Engine{}, nil
+}
+
+func (e *EngineStore) UpdateEngine(ctx context.Context, id string, engineReq *models.EngineRequest) (models.Engine, error) {
+	// TODO: Implement engine update logic
+	return models.Engine{}, nil
+}
+
+
+func (e *EngineStore) DeleteEngine(ctx context.Context, id string) error {
+	// TODO: Implement engine deletion logic
+	return nil
+}
